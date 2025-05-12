2c20352903f02acd653dd02d94b36e78564050c1
Navya17 <navyamade152@gmail.com>
2025-05-12 12:20:57 +0000

    operations commited

--- o1_basics/03.conversions_operations.js
+++ o1_basics/03.conversions_operations.js
@@ -31,6 +31,28 @@ console.log(stringNumber);
 console.log(typeof stringNumber);
 
 
+//*******************operations*****************************//
+
+let value = 3
+let negValue = -value
+console.log(negValue)
 
 
+let str1 = "hello"
+let str2 = " navya"
 
+let str3 = str1 + str2
+console.log(str3)
+
+
+console.log("1"+ 2)
+console.log("1"+ "2")
+console.log("1"+ "2")
+console.log("1" + 2 + 2)
+console.log( 2 + 2+ "2")
+
+// the first type of the value will be considered
+//=> in 2 + 2 + "1" 2 is int so 2 + 2 = 4 is int but "2" is string so 42
+// => in "1" is string so 2 + 2 will be 122 string addition
+
+console.log(true)
