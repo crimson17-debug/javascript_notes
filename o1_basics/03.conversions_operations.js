7bd7ca8ba003ba6e7f56119fc83593f9e8851f1e
Navya17 <navyamade152@gmail.com>
2025-05-12 10:57:18 +0000

    conversions of datatypes

---
+++ o1_basics/03.conversions_operations.js
@@ -0,0 +1,36 @@
+let score = 123
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)// Number
+console.log(valueInNumber); // NaN
+
+//"33" => 33
+//"33avdc" => NaN but type depends on conversion
+//true = 1 , false = 0
+// null = 0
+//undefined = NaN
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1 = true;
+// 0 = false;
+// "" => false;
+//"hitesh" => true;
+
+
+let Number1 = 33
+
+let stringNumber = String(Number1)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
